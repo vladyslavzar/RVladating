2962c143ee9573cd191b769fefd1b2ef2983fce1
Belokan <efendievravil@gmail.com>
2022-11-21 00:06:26 +0100

    added server and client communiation

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({testArray: ["rvl", "rvl1", "rvl2", "rvl3"]});
+    res.json({testArray: ["rvl", "rvl1", "rvl2", "rvl3", "rvl4"]});
 });
 
 app.listen(5000);
